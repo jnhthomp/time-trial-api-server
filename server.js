0a2cc77562f260dacfcd815528d90a62ee715f90
Jonah <jnh.thompson@gmail.com>
2022-06-10 21:20:51 -0400

    Add GET and POST routes for times at a given track
    Can retrieve a sorted list of all times at a given track
    Can POST a new time to a track

--- server.js
+++ server.js
@@ -38,20 +38,23 @@ const requestLogger = (req, res, nxt) => {
 }
 
 app.set('view engine', 'ejs');
-app.use(requestLogger) // Log request data to console
 app.use(express.static('public')) // Files in public folder don't need routes created
 app.use(express.urlencoded({ extended: true })) // https://stackoverflow.com/a/51844327/9059589 - Allows incoming data to be strings, arrays, or objects with nested objects
 app.use(express.json()) // Parses incoming json requests (such as in a head) and adds them to req.body to be accessed
+app.use(requestLogger) // Log request data to console //!Must be after urlencoded() && json() to output body content for objects
 
 /**
  * *Routes
  *  '!' indicates level of importance
- * 
- *! - /api => return list of all games, each containing their own tracks, who each have leaderboards
- *! - /api/:game GET => Return list of all tracks for that game (if it exists)
- *  - /api/:game POST => Create a new game db
+ *  '*' for complete routes
+ *  '?' for notes about the above route
+ **  - /api => return list of all games, each containing their own tracks, who each have leaderboards
+ **  - /api/:game GET => Return list of all tracks for that game (if it exists)
+ *! - /api/:game POST => Create a new game db
+ *      ?Will likely need a track/time object in order to create
  *!!- /api/:game/:trackName GET => Return data for that track such as trackname, image, and laptimes
- *!!- /api/:game/:trackName PUT => Add a time to a tracks leaderboard (Include object with driverInitials and time in req.body)
+ *      ?Starting with just laptimes... Consider creating entry with id: 'about' or something to access a document containing track data
+ **  - /api/:game/:trackName POST => Add a time to a tracks leaderboard (Include object with driverInitials and time in req.body)
  */
 // +Return list of all games (all databases available to client connection)
 app.get('/', (req, res) => { 
@@ -63,6 +66,7 @@ app.get('/', (req, res) => {
     })
 })
 
+// Duplicate route for root and api root will help keep main app and api side to be seperate in the future as this will be phased to only return json
 app.get('/api', (req, res) => { 
   client.db().admin().listDatabases()
     .then((dbs) => { 
@@ -76,16 +80,69 @@ app.get('/api', (req, res) => {
 app.get('/api/:gameName', (req, res) => { 
   client.db(req.params.gameName).collections()
     .then((tracks) => { 
+      // Map through array of tracks
       tracks.map((t) => { 
         console.log(t.collectionName)
+        // Add a name property to display for ejs template (renaming keeps ejs template consistent)
         return t.name = t.collectionName
       })
-      
+      // Path variable to be displayed in ejs
+      // Helps build path to view data on the given game
       let path = `/api/${req.params.gameName}/`
+      
       res.render('path_select.ejs', { info: tracks, type: 'Track', path})
       res.end()
     });
 })
+
+// +Return a list of times for a given game+track
+app.get('/api/:gameName/:trackName', async (req, res) => { 
+  // Return for all times for a given game+track and sort descending by time (fastest first)
+  const data = await client.db(req.params.gameName).collection(req.params.trackName).find().sort({time: 1}).limit(Number.MAX_SAFE_INTEGER).toArray()
+  // Convert seconds from number to string for viewing
+  data.map((el) => { el.time = convertSecToTimeString(el.time) }) // 83.456 => '1:23.456'
+
+  // Log/render found data from search
+  console.log(`${data.length} leaderboard records found for ${req.params.gameName} at ${req.params.trackName}`)
+  console.log(data)
+  res.render('track_leaderboard.ejs', {info: data, game: req.params.gameName, track: req.params.trackName})
+})
+
+// +Add a new time to the leaderboard for a given track/
+app.post('/api/:gameName/:trackName', async (req, res) => { 
+  // Convert time from '1:23.456' string format to seconds as a number format // =>(83.456)
+  const seconds = convertTimeStringToNum(req.body.time) // '1:23.456' => 83.456
+  // Add 
+  const result = await client.db(req.params.gameName).collection(req.params.trackName).insertOne({driverInitial: req.body.driverInitial, time: seconds})
+
+  console.log(`New time submitted to ${req.params.gameName} - ${req.params.trackName} leaderboard with id:${result.insertedId}}`)
+})
+
+/**
+ * *Tools
+ */
+// +Convert seconds (stored as double in database) to a timestamp like string
+// useful for returning data in an expected format or convert for ejs display sake
+// convertSecToTimeString(95.123) => '1:35.123'
+function convertSecToTimeString(secAsNum){
+  const seconds = (secAsNum % 60).toFixed(3);
+  const minutes = Math.floor(secAsNum / 60);
+  const timeStr = `${minutes}:${seconds}`
+  return timeStr
+}
+
+// +Convert seconds timestamp (displayed as string) to a number for storage in database
+// Storing time as a single number of seconds in the database makes it easier to sort results when retrieving
+// convertTimeStringToNum('1:23.456') => 83.456
+function convertTimeStringToNum(timeAsStr){
+  const timeArr = timeAsStr.split(':');
+  const minutes = +timeArr[0];
+  let seconds = +timeArr[1];
+
+  seconds += (minutes * 60)
+
+  return seconds
+}
 
 app.listen(process.env.PORT, () => { 
   console.log(`Server running on port ${process.env.PORT}`)
