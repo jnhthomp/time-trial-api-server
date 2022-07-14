30c1fab164acd0357894ede89d7d93e35a0dadf1
Jonah <jnh.thompson@gmail.com>
2022-07-13 23:24:47 -0700

    Render fetched tracks to the screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [gameData, setGameData] = useState({type: null, data:[]})
 
   useEffect(() => {
+    // Initial fetch to get list of games
     fetch('/api')
-      .then(r => r.json())
-      .then(d => console.log(d))
-      .catch(e => console.log(e))  
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setGameData({
+          type: 'games',
+          data
+        })
+      })
+      .catch(error => console.log(error))  
   }, [])
+
+
   
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {gameData.data.length === 0 ?
+            <li>nothing...</li> :
+            gameData.data.map((data, i) => { 
+              return <li key={i}>{data.name}</li>
+             })
+          }
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
