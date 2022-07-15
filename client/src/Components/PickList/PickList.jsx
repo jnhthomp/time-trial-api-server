8964bd551c498c7af2e61ccec8d5fb0d96700d0b
Jonah <jnh.thompson@gmail.com>
2022-07-14 22:26:08 -0700

    Add <PickList>
    Accepts an object containing game or tracks lists
    Will output a list to display along with buttons to select a given game
    or track list

---
+++ client/src/Components/PickList/PickList.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const PickList = (props) => {
+  console.log(props.list)
+  
+  const selectListItem = (itemName, itemType) => {
+    props.fetchListItem(itemName, itemType)
+  }
+  const items = props.list.data.map((item, index) => { 
+    return (
+      <li key={`${index}-${item.name}`}>{item.name} <button onClick={() => selectListItem(item.name, props.list.type)}>Select</button></li>
+    )
+  })
+
+  return (
+    <ul>
+      {items}
+    </ul>
+  )
+}
+
+export default PickList
