c3c8de84e3b191fd32cd4953ab04bf3a20d8658b
Jonah <jnh.thompson@gmail.com>
2022-07-16 23:59:43 -0700

    Style app title and show form button

--- client/src/Components/MainHeader/MainHeader.jsx
+++ client/src/Components/MainHeader/MainHeader.jsx
@@ -6,7 +6,7 @@ const MainHeader = (props) => {
     <header className={classes['main-header']}>
       <ul>
         <li className={classes.headerHide}></li>{/* new; invisible spacer item */}
-        <li onClick={props.titleClick}>Time Trials App</li>
+        <li className={classes.headerCenter} onClick={props.titleClick}>Time Trials App</li>
         <li className={classes.headerBtn} onClick={props.showForm}>Add Time</li>
       </ul>
     </header>
